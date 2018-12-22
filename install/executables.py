d0591eede8ac81c87f00c7918fce005a970068b1
joaomcteixeira <joaomcteixeira@gmail.com>
2018-12-22 18:04:49 +0100

    improved updater

--- install/executables.py
+++ install/executables.py
@@ -72,6 +72,7 @@ if sys.version_info[0] != 3:
 from install import logger
 from install import messages
 from install import system
+from install import executables
 
 try:
     import installation_vars
@@ -142,8 +143,10 @@ from install import condamanager
 upf = updater.Updater(install_dir)
 upf.run()
 
-# reloads the updated version of system lib
+# reloads the libs updated version
 importlib.reload(system)
+importlib.reload(executables)
+importlib.reload(commons)
 
 log.info("* Checking Conda environment...")
 
